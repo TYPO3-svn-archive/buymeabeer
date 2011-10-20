c9c169575014d98027dd019ead7917963328394b
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-20 21:16:20 +0000

    Fix rawUrlEncode of template.typolink (#31153)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/buymeabeer/trunk@53221 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "buymeabeer".
 #
-# Auto generated 13-10-2011 23:49
+# Auto generated 20-10-2011 23:15
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.4',
+	'version' => '1.0.5',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:19:{s:12:"ext_icon.gif";s:4:"2ddc";s:17:"ext_localconf.php";s:4:"f70e";s:14:"ext_tables.php";s:4:"e9d4";s:15:"flexform_ds.xml";s:4:"6fe4";s:13:"locallang.xml";s:4:"faf6";s:16:"locallang_db.xml";s:4:"60f8";s:12:"t3jquery.txt";s:4:"c68a";s:14:"doc/manual.sxw";s:4:"9cda";s:14:"pi1/ce_wiz.png";s:4:"3fca";s:31:"pi1/class.tx_buymeabeer_pi1.php";s:4:"b319";s:39:"pi1/class.tx_buymeabeer_pi1_wizicon.php";s:4:"b15b";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"fe0a";s:24:"res/tx_buymeabeer_pi1.js";s:4:"946d";s:19:"res/images/beer.png";s:4:"d20e";s:33:"res/jquery/js/jquery-1.6.4.min.js";s:4:"9118";s:34:"res/jquery/js/jquery.easing-1.3.js";s:4:"6516";s:20:"static/constants.txt";s:4:"db0d";s:16:"static/setup.txt";s:4:"d7d6";}',
+	'_md5_values_when_last_written' => 'a:19:{s:12:"ext_icon.gif";s:4:"2ddc";s:17:"ext_localconf.php";s:4:"f70e";s:14:"ext_tables.php";s:4:"e9d4";s:15:"flexform_ds.xml";s:4:"6fe4";s:13:"locallang.xml";s:4:"faf6";s:16:"locallang_db.xml";s:4:"60f8";s:12:"t3jquery.txt";s:4:"c68a";s:14:"doc/manual.sxw";s:4:"de89";s:14:"pi1/ce_wiz.png";s:4:"3fca";s:31:"pi1/class.tx_buymeabeer_pi1.php";s:4:"b319";s:39:"pi1/class.tx_buymeabeer_pi1_wizicon.php";s:4:"b15b";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"fe0a";s:24:"res/tx_buymeabeer_pi1.js";s:4:"946d";s:19:"res/images/beer.png";s:4:"d20e";s:33:"res/jquery/js/jquery-1.6.4.min.js";s:4:"9118";s:34:"res/jquery/js/jquery.easing-1.3.js";s:4:"6516";s:20:"static/constants.txt";s:4:"db0d";s:16:"static/setup.txt";s:4:"0919";}',
 	'suggests' => array(
 	),
 );
