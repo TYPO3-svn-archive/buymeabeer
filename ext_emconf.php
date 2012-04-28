37c9138ac53bccbdc66391185c0c0ddc2de39830
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-28 21:55:42 +0000

    Add jQuery 1.7.2 / Remove deprecated methods (#36599)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/buymeabeer/trunk@61238 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "buymeabeer".
 #
-# Auto generated 27-11-2011 17:31
+# Auto generated 28-04-2012 22:57
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.6',
+	'version' => '1.0.7',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:12:"ext_icon.gif";s:4:"2ddc";s:17:"ext_localconf.php";s:4:"f70e";s:14:"ext_tables.php";s:4:"e9d4";s:15:"flexform_ds.xml";s:4:"6fe4";s:13:"locallang.xml";s:4:"faf6";s:16:"locallang_db.xml";s:4:"60f8";s:12:"t3jquery.txt";s:4:"c68a";s:14:"doc/manual.sxw";s:4:"8c76";s:40:"lib/class.tx_buymeabeer_pagerenderer.php";s:4:"d570";s:14:"pi1/ce_wiz.png";s:4:"3fca";s:31:"pi1/class.tx_buymeabeer_pi1.php";s:4:"77c5";s:39:"pi1/class.tx_buymeabeer_pi1_wizicon.php";s:4:"b15b";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"fe0a";s:24:"res/tx_buymeabeer_pi1.js";s:4:"946d";s:19:"res/images/beer.png";s:4:"d20e";s:33:"res/jquery/js/jquery-1.7.1.min.js";s:4:"ddb8";s:34:"res/jquery/js/jquery.easing-1.3.js";s:4:"6516";s:20:"static/constants.txt";s:4:"996b";s:16:"static/setup.txt";s:4:"0919";}',
+	'_md5_values_when_last_written' => 'a:20:{s:12:"ext_icon.gif";s:4:"2ddc";s:17:"ext_localconf.php";s:4:"f70e";s:14:"ext_tables.php";s:4:"e9d4";s:15:"flexform_ds.xml";s:4:"6fe4";s:13:"locallang.xml";s:4:"faf6";s:16:"locallang_db.xml";s:4:"60f8";s:12:"t3jquery.txt";s:4:"c68a";s:14:"doc/manual.sxw";s:4:"b807";s:40:"lib/class.tx_buymeabeer_pagerenderer.php";s:4:"ac4c";s:14:"pi1/ce_wiz.png";s:4:"3fca";s:31:"pi1/class.tx_buymeabeer_pi1.php";s:4:"77c5";s:39:"pi1/class.tx_buymeabeer_pi1_wizicon.php";s:4:"b15b";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"fe0a";s:24:"res/tx_buymeabeer_pi1.js";s:4:"946d";s:19:"res/images/beer.png";s:4:"d20e";s:33:"res/jquery/js/jquery-1.7.2.min.js";s:4:"b8d6";s:34:"res/jquery/js/jquery.easing-1.3.js";s:4:"6516";s:20:"static/constants.txt";s:4:"c9a7";s:16:"static/setup.txt";s:4:"0919";}',
 	'suggests' => array(
 	),
 );
