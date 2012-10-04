3f19a8eb5d3c29ebd9fdc531aeedd95711996907
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-04 20:21:48 +0000

    Fix “Add new” in backend
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/buymeabeer/trunk@66617 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_buymeabeer_pi1_wizicon.php
+++ pi1/class.tx_buymeabeer_pi1_wizicon.php
@@ -61,13 +61,18 @@ class tx_buymeabeer_pi1_wizicon
 	 *
 	 * @return	The array with language labels
 	 */
-	public function includeLocalLang()
+	protected function includeLocalLang()
 	{
-		$llFile = t3lib_extMgm::extPath('buymeabeer').'locallang.xml';
-		if (class_exists(t3lib_l10n_parser_Llxml)) {
-			$LOCAL_LANG = t3lib_l10n_parser_Llxml::getParsedData($llFile, $GLOBALS['LANG']->lang);
-		} else {
+		$llFile = t3lib_extMgm::extPath('buymeabeer') . 'locallang.xml';
+		$version = class_exists('t3lib_utility_VersionNumber')
+				? t3lib_utility_VersionNumber::convertVersionNumberToInteger(TYPO3_version)
+				: t3lib_div::int_from_ver(TYPO3_version);
+		if ($version < 4006000) {
 			$LOCAL_LANG = t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
+		} else {
+			/** @var $llxmlParser t3lib_l10n_parser_Llxml */
+			$llxmlParser = t3lib_div::makeInstance('t3lib_l10n_parser_Llxml');
+			$LOCAL_LANG = $llxmlParser->getParsedData($llFile, $GLOBALS['LANG']->lang);
 		}
 
 		return $LOCAL_LANG;
