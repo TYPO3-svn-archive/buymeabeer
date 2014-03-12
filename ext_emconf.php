fa9db17f47294b52925389304f03ebd01416260c
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-03-12 20:33:08 +0000

    Add Compatibility for TYPO3 V 6.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/buymeabeer/trunk@84226 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,49 +1,48 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "buymeabeer".
-#
-# Auto generated 04-10-2012 22:21
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "buymeabeer".
+ *
+ * Auto generated 12-03-2014 21:31
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Buy me a beer',
 	'description' => 'Insert a link with the payment information for donating (e.g. PayPal). Use t3jquery for better integration with other jQuery extensions.',
 	'category' => 'plugin',
-	'author' => 'Juergen Furrer',
-	'author_email' => 'juergen.furrer@gmail.com',
-	'shy' => '',
-	'dependencies' => 'cms',
+	'shy' => 0,
+	'version' => '1.0.11',
+	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
 	'module' => '',
 	'state' => 'stable',
-	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 1,
+	'clearcacheonload' => 1,
 	'lockType' => '',
+	'author' => 'Juergen Furrer',
+	'author_email' => 'juergen.furrer@gmail.com',
 	'author_company' => '',
-	'version' => '1.0.10',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'cms' => '',
 			'php' => '5.0.0-0.0.0',
-			'typo3' => '4.3.0-0.0.0',
+			'typo3' => '4.3.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:12:"ext_icon.gif";s:4:"2ddc";s:17:"ext_localconf.php";s:4:"f70e";s:14:"ext_tables.php";s:4:"e9d4";s:15:"flexform_ds.xml";s:4:"6fe4";s:13:"locallang.xml";s:4:"faf6";s:16:"locallang_db.xml";s:4:"60f8";s:12:"t3jquery.txt";s:4:"c68a";s:14:"doc/manual.sxw";s:4:"53b2";s:40:"lib/class.tx_buymeabeer_pagerenderer.php";s:4:"373d";s:14:"pi1/ce_wiz.png";s:4:"3fca";s:31:"pi1/class.tx_buymeabeer_pi1.php";s:4:"77c5";s:39:"pi1/class.tx_buymeabeer_pi1_wizicon.php";s:4:"bba9";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"fe0a";s:24:"res/tx_buymeabeer_pi1.js";s:4:"946d";s:19:"res/images/beer.png";s:4:"d20e";s:33:"res/jquery/js/jquery-1.7.2.min.js";s:4:"b8d6";s:34:"res/jquery/js/jquery.easing-1.3.js";s:4:"6516";s:20:"static/constants.txt";s:4:"c9a7";s:16:"static/setup.txt";s:4:"0919";}',
-	'suggests' => array(
-	),
+	'_md5_values_when_last_written' => 'a:20:{s:12:"ext_icon.gif";s:4:"2ddc";s:17:"ext_localconf.php";s:4:"f70e";s:14:"ext_tables.php";s:4:"e9d4";s:15:"flexform_ds.xml";s:4:"6fe4";s:13:"locallang.xml";s:4:"faf6";s:16:"locallang_db.xml";s:4:"60f8";s:12:"t3jquery.txt";s:4:"c68a";s:14:"doc/manual.sxw";s:4:"756f";s:40:"lib/class.tx_buymeabeer_pagerenderer.php";s:4:"373d";s:14:"pi1/ce_wiz.png";s:4:"3fca";s:31:"pi1/class.tx_buymeabeer_pi1.php";s:4:"2a69";s:39:"pi1/class.tx_buymeabeer_pi1_wizicon.php";s:4:"bba9";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"fe0a";s:24:"res/tx_buymeabeer_pi1.js";s:4:"946d";s:19:"res/images/beer.png";s:4:"d20e";s:33:"res/jquery/js/jquery-1.7.2.min.js";s:4:"b8d6";s:34:"res/jquery/js/jquery.easing-1.3.js";s:4:"6516";s:20:"static/constants.txt";s:4:"c9a7";s:16:"static/setup.txt";s:4:"0919";}',
 );
 
 ?>
